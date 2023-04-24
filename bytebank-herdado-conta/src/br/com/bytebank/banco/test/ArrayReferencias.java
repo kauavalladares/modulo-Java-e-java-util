f827513616276391836f6d538a7ec15b1a6ced21
Kauavalladares <kaua.valldares@gmail.com>
2023-04-24 13:31:54 -0300

    adição dos arquivos pra realização dos exercicios.

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/ArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/ArrayReferencias.java
@@ -5,11 +5,20 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 public class ArrayReferencias {
     public static void main(String[] args) {
 
+
         ContaCorrente [] contas = new ContaCorrente[5];
-
         ContaCorrente cc1 = new ContaCorrente(22,11);
-        ContaCorrente cc2 = new ContaCorrente(22,22);
+        contas[0] = cc1;
 
+        ContaCorrente  cc2 = new ContaCorrente(22,22);
+        contas[1] = cc2;
+
+        System.out.println( contas[1].getNumero() );
+
+        ContaCorrente ref = contas[1];
+        System.out.println(cc1.getNumero());
+        System.out.println(ref.getNumero());
+        
         contas[0] = cc1;
         contas[1] = cc2;
     }
